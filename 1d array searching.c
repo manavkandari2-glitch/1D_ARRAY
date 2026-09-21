0f897dca8db319b5112789e2c1f4c314d352f825
manavkandari2-glitch <manavkandari2@gmail.com>
2026-09-21 21:34:34 +0530

    Add files via upload

---
+++ 1d array searching.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int Search(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[100] = {1, 2, 3, 4, 5};
+    int n = 5;
+    int key = 3;
+
+    int index = Search(arr, n, key);
+
+    if (index != -1) {
+        printf("Element %d found at index %d\n", key, index);
+    } else {
+        printf("Element %d not found in the array\n", key);
+    }
+
+    return 0;
+}
